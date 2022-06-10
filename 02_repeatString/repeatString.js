f3f15ab5f1a20bf773ad1fb9dbcbe492b662629e
An Pham <andypham0227@gmail.com>
2022-06-10 15:21:56 -0700

    Did lessons 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (hey, number) {
+  if (number < 0) {
+    return 'ERROR';
+  }
+  let string = '';
+  string = `${hey.repeat(number)}`;
+  return string;
 };
 
 // Do not edit below this line
